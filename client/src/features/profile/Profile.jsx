ee0423dde4cb44dcdcafffbb851940d616953e05
coderoadsoftwares <coderoadsoftwares@gmail.com>
2025-10-11 23:17:33 +0530

    Enhance Profile component to display user details from API
    - Integrated the `useGetUserProfileQuery` hook to fetch and display user profile information, including name, contact details, plan, API key, and account status.
    - This update improves the user interface by providing comprehensive profile data directly within the Profile component.

--- client/src/features/profile/Profile.jsx
+++ client/src/features/profile/Profile.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useGetUserProfileQuery } from "./api/profileApi";
 
 function Profile() {
-  return <div>Profile</div>;
+  const { data } = useGetUserProfileQuery();
+
+  return (
+    <div>
+      <p>
+        Name: {data?.firstName} {data?.lastName}
+      </p>
+      <p>Phone: {data?.phone}</p>
+      <p>Email: {data?.email}</p>
+      <p>Plan: {data?.plan}</p>
+      <p>API Key: {data?.apiKey}</p>
+      <p>Is Active: {data?.isActive ? "Yes" : "No"}</p>
+      <p>Last Login: {data?.lastLogin}</p>
+      <p>Created At: {data?.createdAt}</p>
+      <p>Updated At: {data?.updatedAt}</p>
+    </div>
+  );
 }
 
 export default Profile;
